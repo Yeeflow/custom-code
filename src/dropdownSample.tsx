0a477efb85f69845f616b7c83c592c0c50db3be5
Frank Lu <frank.lu@akmii.com>
2020-09-10 20:26:52 +0800

    fix dropdown

--- src/dropdownSample.tsx
+++ src/dropdownSample.tsx
@@ -22,26 +22,7 @@ class DropdownSample extends React.Component<DropdownSampleProps, DropdownSample
 
 
     componentWillMount() {
-        const { context } = this.props;
-        const common = context.modules[MODULE_COMMON]
-        const { Request } = common;
-        var url = "https://apps.yeeflow.com/YeeOfficeSettings/_API/Ver(3.0)/api/crafts/datas/listid";
-        var data = {
-            AppID: 41,
-            ListID: "1264749353964408835",
-            columns: ["Text2"],
-            pageIndex: 1,
-            pageSize: 100,
-        };
-        var req = new Request();
-        req.post(url, data).then((rs) => {
-            if (rs && rs.Status === 0 && rs.Data) {
-                const options = rs.Data.map(d => {
-                    return d.Text2;
-                })
-                this.setState({ options });
-            }
-        });
+        this.setState({ options: ["a", "b", "c"] });
     }
 
     onChange(v) {
